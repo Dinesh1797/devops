bc0cf3f278dbb5e2b692f7fd5ac6dc56466f15f6
Dinesh1797 <dineshayyadurai97@gmail.com>
2024-12-10 13:04:15 +0530

    Initial commit: Node.js app, Dockerfile, and Terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,3 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "pav-terraform-state-bucket"
-#     key    = "ecs/devops-app/terraform.tfstate"
-#     region = "ap-northeast-1"
-#   }
-# }
-
 provider "aws" {
   region = var.region
 }
