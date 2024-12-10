fce32567455da89034065748a73d666dbf8acd06
Dinesh1797 <dineshayyadurai97@gmail.com>
2024-12-10 14:56:12 +0530

    Initial commit: Node.js app, Dockerfile, and Terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ resource "aws_ecs_cluster" "cluster" {
 }
 
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole-${random_id.unique_id.hex}"
+  name = "ecsTaskExecutionRole"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
